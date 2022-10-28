1c73db32779e67f1b4fe4d50f010d1d4ef376e0a
Action52 <Action52>
2022-10-28 07:37:38 +0000

    Fix queries

--- scripts/queries_generated/queries_3G_Fixed.sql
+++ scripts/queries_generated/queries_3G_Fixed.sql
@@ -3709,12 +3709,7 @@ select i_brand_id brand_id, i_brand brand,t_hour,t_minute,
 
 -- end query 71 in stream 0 using template query71.tpl
 -- start query 72 in stream 0 using template query72.tpl
-select  i_item_desc
-      ,w_warehouse_name
-      ,d1.d_week_seq
-      ,sum(case when p_promo_sk is null then 1 else 0 end) no_promo
-      ,sum(case when p_promo_sk is not null then 1 else 0 end) promo
-      ,count(*) total_cnt
+select i_item_desc, w_warehouse_name, d1.d_week_seq, sum(case when p_promo_sk is null then 1 else 0 end) no_promo, sum(case when p_promo_sk is not null then 1 else 0 end) promo, count(*) total_cnt
 from catalog_sales
 join inventory on (cs_item_sk = inv_item_sk)
 join warehouse on (w_warehouse_sk=inv_warehouse_sk)
